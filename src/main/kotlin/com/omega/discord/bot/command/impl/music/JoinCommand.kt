8aa4a1b9e22f6fafe4a6a2adc351536144ba26f0
KyuBlade <jonesadev@gmail.com>
2017-10-06 19:55:12 +0200

    Added message when bot join a voice channel

--- src/main/kotlin/com/omega/discord/bot/command/impl/music/JoinCommand.kt
+++ src/main/kotlin/com/omega/discord/bot/command/impl/music/JoinCommand.kt
@@ -12,30 +12,34 @@ class JoinCommand : Command {
 
     override val name: String = "join"
     override val aliases: Array<String>? = arrayOf("j")
-    override val usage: String = "**join** - Join your current voice channel\r\n" +
+    override val usage: String = "**join** - Join your current voice channel\n" +
             "**join <voice_channel_name>** - Join the named voice channel"
     override val allowPrivate: Boolean = false
     override val permission: Permission = Permission.COMMAND_JOIN
     override val ownerOnly: Boolean = false
 
     override fun execute(author: IUser, channel: IChannel, message: IMessage, args: List<String>) {
+
         val voiceChannel: IVoiceChannel?
 
         if (args.isNotEmpty()) {
+
             val channelName = args.joinToString(" ")
 
             voiceChannel = message.guild.getVoiceChannelsByName(channelName).firstOrNull()
 
             if (voiceChannel == null)
                 MessageSender.sendMessage(channel, "No voice channels named $channelName found")
+
         } else {
+
             voiceChannel = author.getVoiceStateForGuild(channel.guild).channel
 
             if (voiceChannel == null)
                 MessageSender.sendMessage(channel, "You are not in a voice channel")
         }
 
-        if (voiceChannel != null) {
+        voiceChannel?.let {
 
             when {
                 !PermissionUtils.hasPermissions(voiceChannel, channel.client.ourUser, Permissions.VOICE_CONNECT) ->
@@ -45,7 +49,11 @@ class JoinCommand : Command {
                     MessageSender.sendMessage(channel, "Already connected to this channel")
 
                 else ->
-                    RequestBuffer.request { voiceChannel.join() }
+                    RequestBuffer.request {
+
+                        voiceChannel.join()
+                        MessageSender.sendMessage(channel, "Joined channel **${voiceChannel.name}**")
+                    }
             }
         }
     }
